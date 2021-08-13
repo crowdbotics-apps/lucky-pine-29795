34f4e21c6fc2ad3eb14fa00bb1251a72bcde5ad4
Crowdbotics <team@crowdbotics.com>
2021-08-13 21:33:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "lucky_pine_29795Identifier",
+  url: "https://lucky_pine_29795Identifier.botics.co",
+  api: "https://lucky_pine_29795Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
